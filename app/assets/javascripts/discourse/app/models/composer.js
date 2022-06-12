08cfa72963a7e33a51311b3810fa18a44fe3d760
Jarek Radosz <jradosz@gmail.com>
2022-06-12 23:46:17 +0200

    restore another

--- app/assets/javascripts/discourse/app/models/composer.js
+++ app/assets/javascripts/discourse/app/models/composer.js
@@ -15,6 +15,7 @@ import I18n from "I18n";
 import { Promise } from "rsvp";
 import { QUOTE_REGEXP } from "discourse/lib/quote";
 import RestModel from "discourse/models/rest";
+import Site from "discourse/models/site";
 import Topic from "discourse/models/topic";
 import User from "discourse/models/user";
 import bootbox from "bootbox";
@@ -1332,6 +1333,7 @@ Composer.reopenClass({
   create(args) {
     args = args || {};
     args.user = args.user || User.current();
+    args.site = args.site || Site.current();
     return this._super(args);
   },
 
