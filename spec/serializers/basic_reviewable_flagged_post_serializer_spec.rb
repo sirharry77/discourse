a17187e020a5f7744b53740f7c66fa28e395128d
OsamaSayegh <asooomaasoooma90@gmail.com>
2022-06-24 07:08:19 +0300

    add specs for basic reviewable and flagged post serializers

---
+++ spec/serializers/basic_reviewable_flagged_post_serializer_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+describe BasicReviewableFlaggedPostSerializer do
+  fab!(:topic) { Fabricate(:topic, title: "unsafe title <a>") }
+  fab!(:post) { Fabricate(:post, topic: topic) }
+
+  fab!(:reviewable) do
+    ReviewableFlaggedPost.needs_review!(target: post, topic: topic, created_by: Discourse.system_user)
+  end
+
+  def get_json
+    described_class.new(reviewable, root: false).as_json
+  end
+
+  it "includes post_number of the flagged post" do
+    expect(get_json[:post_number]).to eq(post.post_number)
+  end
+
+  it "includes topic_title of the flagged post" do
+    expect(get_json[:topic_title]).to eq("Unsafe title &lt;a&gt;")
+  end
+end
